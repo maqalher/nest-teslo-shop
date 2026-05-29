35fd4b6500d458be2c0d646b06e8234043c6a1e0
Alan Maqueda <maqalher@hotmail.com>
2026-05-29 14:35:37 -0600

    first commit

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,33 @@
+import { ProductsService } from './../products/products.service';
+import { Injectable } from '@nestjs/common';
+import { initialData } from './data/seed-data';
+
+@Injectable()
+export class SeedService {
+
+  constructor(
+    private readonly productsService: ProductsService
+  ){}
+
+  async runSeed() {
+    await this.insertNewProducts()
+
+    return 'This action adds a new seed';
+  }
+
+  private async insertNewProducts() {
+    await this.productsService.deleteAllProducts()
+
+    const products = initialData.products
+
+    const insertPromises: Promise<any>[] = []
+
+    products.forEach(product => {
+      insertPromises.push(this.productsService.create(product))
+    })
+
+    await Promise.all(insertPromises)
+
+    return true
+  }
+}
